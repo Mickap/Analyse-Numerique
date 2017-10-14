c9faf6fec92fabbadb627dc5eb76c125718a294b
joevin <joevin.soulenq@gmail.com>
2017-10-14 22:20:20 +0200

    calculNewton() simplifié

--- traitement.cpp
+++ traitement.cpp
@@ -7,31 +7,31 @@
 void Traitement::calculNewton(double pas)
 {
     QVector<QVector<double> > mat_diff_div; // tableau des différences divisés
-    double min=0;                       // valeur minimum
-    double max=10/pas;           // valeur maximum
-    double nb_points_interp=(max-min)+1;    // nombre de points interpolés
-    nb_points = Xselec.size();              // nombre de points fournis
-    int n=nb_points - 1;                    // indice du dernier élément de X1
-    double val_polynome;                    // contiendra chaque point du polynome
+    double min=0.0;             // valeur minimum
+    double max=10.0;            // valeur maximum
+    double y, x=min;            // contiendra chaque points du polynome
+    nb_points = Xselec.size();  // nombre de points fournis
+    int n=nb_points - 1;        // indice du dernier élément de X1
 
-    Xinterp.resize(nb_points_interp);
-    Yinterp.resize(nb_points_interp);
 
     qDebug() << "Calcul des différences divisées :";
     mat_diff_div = DifferencesDivisees();
 
     qDebug() << "Calcul du polynôme d'interpolation :";
-    for (int i = 0; min <= max; min++, i++) {
+    Xinterp.clear(); Yinterp.clear(); // on efface le tableau
+    for (int i = 0; x <= max; x+=pas, i++) {
+
         // Calcul des points Xinterp d'interpolation
-        Xinterp[i] = pas*min;
+        Xinterp.append(x);
 
         // Calcul des points Yinterp d'interpolation
-        val_polynome = mat_diff_div[0][n];
-        for (int j = 1; j <= n; j++)
-            val_polynome = val_polynome * (Xinterp[i] - Xselec[n - j]) + mat_diff_div[0][n - j];
-        Yinterp[i] = val_polynome;
+        y = mat_diff_div[0][n];
+        for (int j = 1; j <= n; j++) {
+            y = y * (Xinterp[i] - Xselec[n - j]) + mat_diff_div[0][n - j];
+        }
+        Yinterp.append(y);
     }
-    qDebug() << "Xinterp :" << Xinterp << endl << "Yinterp :" << Yinterp;
+    qDebug() << "Xinterp :" << Xinterp << endl << "Yinterp :" << Yinterp << endl;
 }
 
 /**
@@ -72,12 +72,12 @@ QVector<QVector<double> > Traitement::DifferencesDivisees()
  */
 void Traitement::calculerCourbeInitiale(QString nomFonction)
 {
-    double min=0;    // valeur minimum
-    double max=10;   // valeur maximum
+    double min=0.0;  // valeur minimum
+    double max=10.0; // valeur maximum
     double pas=0.01; // précision
 
     Xinit.clear(); Yinit.clear(); // on efface le tableau
-    for(double i=min; i < max; i+=pas) {
+    for(double i=min; i <= max; i+=pas) {
         Xinit.append(i);
         if(nomFonction == "cos()")  Yinit.append(cos(i*2));
         if(nomFonction == "sin()")  Yinit.append(sin(i*2));
@@ -105,19 +105,21 @@ void Traitement::calculerPointsInitiaux(int nbPointsInterp, bool randomActive)
     Xselec.append(Xinit.first());
     Yselec.append(Yinit.first());
 
-    if(randomActive)
-    {
-
+    // Sélection des points à intervalle aléatoire
+    if(randomActive) {
+        nbPointsInterp-=2;
         int random=0;
         for (i = 0; i < nbPointsInterp; i++) {
             random = rand() % (Xinit.size() / nbPointsInterp) + (Xinit.size() / nbPointsInterp) * i;
             Xselec.append(Xinit[random]);
             Yselec.append(Yinit[random]);
         }
+    }
 
-    } else {
-        // version sans random
-        for (i=1; i < nbPointsInterp; i++) {
+    // Sélection des points à intervalle régulier
+    else {
+        nbPointsInterp--;
+        for (i = 1; i < nbPointsInterp; i++) {
             Xselec.append(Xinit[(i*Xinit.size()/nbPointsInterp)]);
             Yselec.append(Yinit[(i*Xinit.size()/nbPointsInterp)]);
         }
@@ -126,4 +128,6 @@ void Traitement::calculerPointsInitiaux(int nbPointsInterp, bool randomActive)
     // On place un point à la fin de la courbe
     Xselec.append(Xinit.last());
     Yselec.append(Yinit.last());
+
+    qDebug() << "Xselec :" << Xselec << endl << "Yselec :" << Yselec << endl;
 }
